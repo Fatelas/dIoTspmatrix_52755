34c88b1c6d3701393e03e1256b646c0ef27fd327
Fatelas <filipemrks@gmail.com>
2022-04-01 14:12:00 +0100

    changed verification in position_is function, and ran public test

--- position.py
+++ position.py
@@ -1,27 +1,34 @@
-position = tuple[int,int]
+position = tuple[int, int]
 
-def position_create(row:int, col:int) -> position :
-    if not ( type(row) is int and row >= 0 ) or not ( type(col) is int and col >= 0):
+
+def position_create(row: int, col: int) -> position:
+    if not (type(row) is int and row >= 0) or not (type(col) is int and col >= 0):
         raise ValueError('position_create: invalid arguments')
     return row, col
 
-def position_is(pos: position) -> bool :
-    if type(pos) is tuple and type(pos[0]) is int and type(pos[1]) is int:
+
+def position_is(pos: position) -> bool:
+    if type(pos) is tuple and type(pos[0]) is int and type(pos[1]) is int and pos[0] >= 0 and pos[1] >= 0:
         return True
-    else :
+    else:
         return False
 
-def position_row(pos: position) -> int :
+
+def position_row(pos: position) -> int:
     if position_is(pos):
         return pos[0]
-    else : raise ValueError('position_row: invalid arguments')
+    else:
+        raise ValueError('position_row: invalid arguments')
 
-def position_col(pos: position) -> int :
+
+def position_col(pos: position) -> int:
     if position_is(pos):
         return pos[1]
-    else : raise ValueError('position_col: invalid arguments')
+    else:
+        raise ValueError('position_col: invalid arguments')
 
-def  position_equal(pos1: position, pos2: position) -> bool :
+
+def position_equal(pos1: position, pos2: position) -> bool:
     if position_is(pos1) and position_is(pos2):
         if pos1 == pos2:
             return True
@@ -30,12 +37,9 @@ def  position_equal(pos1: position, pos2: position) -> bool :
     else:
         raise ValueError('position_equal: invalid arguments')
 
-def position_str(pos: position) -> str :
-    if position_is(pos) :
+
+def position_str(pos: position) -> str:
+    if position_is(pos):
         return '(' + str(pos[0]) + ', ' + str(pos[1]) + ')'
-    else :
+    else:
         raise ValueError('position_str: invalid arguments')
-
-
-
-
